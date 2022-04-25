578a84c671d54055fa53df78e9ebcaf0a1187235
Adeola Fadare <104224084+LazyCoder4542@users.noreply.github.com>
2022-04-25 21:35:31 +0100

    Update index.js
    email validation

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 window.alert("Welcome to Group 127 's Project Page. Hope you enjoy!");
+function validate() {
+    var input = document.createElement('input');
+    input.type='email';
+    input.value=document.getElementById('email').value;
+      
+    if (input.checkValidity()) {
+        document.getElementById('error').style.display = 'none';
+    } else {
+        document.getElementById('error').style.display = 'block';
+    }
+      
+    return false;
+  }
